1c4fa5d6e22be556f6d8063476dd67088da61b9c
ayush singh <ayushs24101@iiitnr.edu.in>
2025-04-19 17:07:14 +0530

    Update changes

--- index.js
+++ index.js
@@ -11,7 +11,12 @@ dotenv.config({
 const app = express();
 
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true, // if you're using cookies/auth headers
+  })
+);
 app.use(
   express.json({
     limit: "16kb",
